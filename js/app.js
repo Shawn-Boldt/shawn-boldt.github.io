88061c2aff6e9b6fa0051d6d7b0bcdc7ab6ef3f0
Shawn Wieboldt <shawn.wieboldt@gmail.com>
2016-11-11 08:10:17 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// app file, 
+// animates the scrolling when nav items are clicked
+//NOT WORKING!!!! NEED TO CONFIGURE FOR THIS PARTICULAR WEBSITE.
+
+$(document).ready(function(){
+	setAnchors();   //calls the function after the page loads
+
+});
+
+function setAnchors() {  
+	$("nav a").click(function(e){  //$ selects nav and a elements. On click function(e) is executed
+		e.preventDefault(); 
+		var sectionID = e.currentTarget.id + "Section"; //assigns the current target the concatinated string Section
+		
+		$("html body").animate({ //selects html and body and animates
+			scrollTop: $("#" + sectionID).offset().top
+		}, 1000) //animation time is in ms so 1000ms is 1 second.
+	})
+}

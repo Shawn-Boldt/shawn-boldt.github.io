a68134fe63d9ae426c9e62ca8d298d988cf6e0f5
Shawn Wieboldt <shawn.wieboldt@gmail.com>
2016-11-17 09:36:54 -0500

    added meta tags

--- js/app.js
+++ js/app.js
@@ -3,17 +3,19 @@
 //NOT WORKING!!!! NEED TO CONFIGURE FOR THIS PARTICULAR WEBSITE.
 
 $(document).ready(function(){
-	setAnchors();   //calls the function after the page loads
+//	setAnchors();   //calls the function after the page loads
+
+
 
 });
 
-function setAnchors() {  
-	$("nav a").click(function(e){  //$ selects nav and a elements. On click function(e) is executed
-		e.preventDefault(); 
-		var sectionID = e.currentTarget.id + "Section"; //assigns the current target the concatinated string Section
-		
-		$("html body").animate({ //selects html and body and animates
-			scrollTop: $("#" + sectionID).offset().top
-		}, 1000) //animation time is in ms so 1000ms is 1 second.
-	})
-}
+//function setAnchors() {  
+//	$("nav a").click(function(e){  //$ selects nav and a elements. On click function(e) is executed
+//		e.preventDefault(); 
+//		var sectionID = e.currentTarget.id + "Section"; //assigns the current target the concatinated string Section
+//		
+//		$("html body").animate({ //selects html and body and animates
+//			scrollTop: $("#" + sectionID).offset().top
+//		}, 1000) //animation time is in ms so 1000ms is 1 second.
+//	})
+//}
